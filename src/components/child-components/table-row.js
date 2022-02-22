f91bd1699042963891430d207a01e03394431fff
rishi120 <r.baruah42@gmail.com>
2022-02-22 11:44:09 -0800

    created table component with tailwind css classes.

---
+++ src/components/child-components/table-row.js
@@ -0,0 +1,34 @@
+import React from 'react'
+export default function Rendertablerowdata() {
+    return (
+        <tr>
+            <th
+                scope="col"
+                className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+            >
+                Name
+            </th>
+            <th
+                scope="col"
+                className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+            >
+                Title
+            </th>
+            <th
+                scope="col"
+                className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+            >
+                Status
+            </th>
+            <th
+                scope="col"
+                className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+            >
+                Role
+            </th>
+            <th scope="col" className="relative px-6 py-3">
+                <span className="sr-only">Edit</span>
+            </th>
+        </tr>
+    )
+}
---
+++ src/data/table-data.js
@@ -0,0 +1,75 @@
+export const people = [
+    {
+        name: 'Jane Cooper',
+        title: 'Regional Paradigm Technician',
+        department: 'Optimization',
+        role: 'Admin',
+        email: 'jane.cooper@example.com',
+        image:
+            'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
+    },
+    {
+        name: 'Alex Cooper',
+        title: 'Software Engineer',
+        department: 'Software Engineering',
+        role: 'Software Engineer',
+        email: 'alax.cooper@example.com',
+        image:
+            'https://images.unsplash.com/photo-1644862742524-2d96acb5d739?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1413&q=80',
+    },
+    {
+        name: 'Chandan Baruah',
+        title: 'Software Engineer (UI)',
+        department: 'Software Engineering',
+        role: 'Software Engineer',
+        email: 'r.baruah42@example.com',
+        image:
+            'https://images.unsplash.com/photo-1644355304172-26601bcbe3ed?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1472&q=80',
+    },
+    {
+        name: 'John Doe',
+        title: 'Accountant',
+        department: 'Income Tax',
+        role: 'Admin',
+        email: 'john.doe@example.com',
+        image:
+            'https://images.unsplash.com/photo-1643773664163-1b7c307ae888?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1931&q=80',
+    },
+    {
+        name: 'Ravi Kumar',
+        title: 'Cyber Security Specialist',
+        department: 'Networking',
+        role: 'IT',
+        email: 'ravi.kumar@example.com',
+        image:
+            'https://images.unsplash.com/photo-1643717347866-f213892b736b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+    },
+    {
+        name: 'Shreesha Sharma',
+        title: 'Frontend Engineer',
+        department: 'IT',
+        role: 'Engineer',
+        email: 'shreesha.sharma@example.com',
+        image:
+            'https://images.unsplash.com/photo-1643719142162-4406375cbd02?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+    },
+    {
+        name: 'Jane Cooper',
+        title: 'Regional Paradigm Technician',
+        department: 'Optimization',
+        role: 'Admin',
+        email: 'jane.cooper@example.com',
+        image:
+            'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
+    },
+    {
+        name: 'Jane Cooper',
+        title: 'Regional Paradigm Technician',
+        department: 'Optimization',
+        role: 'Admin',
+        email: 'jane.cooper@example.com',
+        image:
+            'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
+    },
+
+]
