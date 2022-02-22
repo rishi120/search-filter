56312dee2f0a2666eee7585841e188e797136896
rishi120 <r.baruah42@gmail.com>
2022-02-22 10:18:39 -0800

    created intial setup and iInstalled tailwind css.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Mastercomponent from "./components/master";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <Mastercomponent/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/master.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Mastercomponent() {
+  return (
+    <div>
+        <h1 className='text-3xl'>master</h1>
+    </div>
+  )
+}
