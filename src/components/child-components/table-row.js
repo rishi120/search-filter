8e35864fc6d83211c3deb8d6a068abee7d08c879
rishi120 <r.baruah42@gmail.com>
2022-02-24 13:42:34 -0800

    added apply tailwind directive.

--- src/components/child-components/table-row.js
+++ src/components/child-components/table-row.js
@@ -5,25 +5,25 @@ export default function Rendertablerowdata() {
         <tr>
             <th
                 scope="col"
-                className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                className="common-table-header"
             >
                 Name
             </th>
             <th
                 scope="col"
-                className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                className="common-table-header"
             >
                 Title
             </th>
             <th
                 scope="col"
-                className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                className="common-table-header"
             >
                 Status
             </th>
             <th
                 scope="col"
-                className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                className="common-table-header"
             >
                 Role
             </th>
